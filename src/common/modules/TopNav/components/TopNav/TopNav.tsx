33fef8e2810dce6adfc1e9fe085c927048c993ed
Adrian Bunge <adrian@nonacreative.com>
2020-04-05 12:52:22 +0200

    feat: add new fonts style: cleanup top nav bar

--- src/common/modules/TopNav/components/TopNav/TopNav.tsx
+++ src/common/modules/TopNav/components/TopNav/TopNav.tsx
@@ -1,6 +1,7 @@
 import classes from 'classnames'
 import React from 'react'
 import { NavigationItem } from '../../types'
+import onGatherLogo from './../../assets/onGatherLogo.svg'
 import styles from './TopNav.module.scss'
 
 export interface Props {
@@ -12,36 +13,35 @@ export const TopNav: React.FunctionComponent<Props> = ({
   handleNavigationItemClicked,
   selectedNavigationItem,
 }) => {
-  const iconStyle = (navigationItem: NavigationItem): object => {
+  const topNavItemStyle = (navigationItem: NavigationItem): object => {
     return {
-      [styles.iconSelected]: selectedNavigationItem === navigationItem,
-      [styles.iconUnselected]: selectedNavigationItem !== navigationItem,
+      [styles.topNavItem]: true,
+      [styles.topNavItemSelected]: selectedNavigationItem === navigationItem,
     }
   }
 
   return (
     <div className={styles.container}>
-      <div
-        className={classes(
-          styles.buttonContainer,
-          iconStyle(NavigationItem.Search),
-        )}
-        onClick={(): void => handleNavigationItemClicked(NavigationItem.Search)}
-        data-testid="searchIconContainer"
-      >
-        <p>Create Gathering</p>
-      </div>
-      <div
-        className={classes(
-          styles.buttonContainer,
-          iconStyle(NavigationItem.Profile),
-        )}
-        onClick={(): void =>
-          handleNavigationItemClicked(NavigationItem.Profile)
-        }
-        data-testid="profileIconContainer"
-      >
-        <p>Login</p>
+      <h1 className={styles.onGatherLogo}>onGather</h1>
+      <div className={styles.callsToAction}>
+        <div
+          className={classes(topNavItemStyle(NavigationItem.Search))}
+          onClick={(): void =>
+            handleNavigationItemClicked(NavigationItem.Search)
+          }
+          data-testid="searchIconContainer"
+        >
+          <p>Create</p>
+        </div>
+        <div
+          className={classes(topNavItemStyle(NavigationItem.Profile))}
+          onClick={(): void =>
+            handleNavigationItemClicked(NavigationItem.Profile)
+          }
+          data-testid="profileIconContainer"
+        >
+          <p>Login</p>
+        </div>
       </div>
     </div>
   )
--- src/modules/Home/components/Home/Home.tsx
+++ src/modules/Home/components/Home/Home.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { HowItWorks } from '../HowItWorks'
+import homeBackgroundImage from './../../assets/homeBackgroundImage.svg'
+import homeBackgroundImage2 from './../../assets/homeBackgroundImage2.png'
 import styles from './Home.module.scss'
 
 export interface Props {
@@ -7,6 +10,30 @@ export interface Props {
 
 export const Home: React.FunctionComponent<Props> = ({ dummyText }) => (
   <div className={styles.container}>
-    <p>{dummyText}</p>
+    <section className={styles.homeSection}>
+      <img
+        src={homeBackgroundImage}
+        alt="home background image"
+        className={styles.homeBackgroundImage}
+      />
+      <h1 className={styles.homePageTitle}>
+        Want to connect with friends
+        <br /> more meaningfully
+        <br /> on video calls?
+      </h1>
+    </section>
+    <section className={styles.homeSection2}>
+      <img
+        src={homeBackgroundImage2}
+        alt="follow icon"
+        className={styles.homeBackgroundImage2}
+      />
+      <h1 className={styles.homePageTitle2}>
+        We&apos;ve got some templates
+        <br /> that will make it easy
+        <br /> to bring everyone together
+      </h1>
+    </section>
+    <HowItWorks dummyText={'also some text'} />
   </div>
 )
