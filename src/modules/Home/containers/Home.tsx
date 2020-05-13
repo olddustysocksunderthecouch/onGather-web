49b23dab3ba781c7b9338eee6908b1cc33fcfc3f
Adrian Bunge <adrian@nonacreative.com>
2020-05-13 14:09:09 +0200

    chore(home): clean up unused props

--- src/modules/Home/containers/Home.tsx
+++ src/modules/Home/containers/Home.tsx
@@ -1,24 +1,16 @@
-import React, { Dispatch } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { AnyAction } from 'redux'
 import TopNavLayout from '../../../common/modules/TopNav/containers/TopNavLayout'
 import { ConnectedReduxProps } from '../../../common/redux/types'
 import { Home } from '../components/Home'
 
-interface Props extends ConnectedReduxProps<AnyAction> {
-  handleClick: () => void
-}
-
-const HomeContainer = ({
-  handleClick,
-}: Props): React.FunctionComponentElement<Props> => (
+const HomeContainer = (): React.FunctionComponentElement<
+  ConnectedReduxProps<AnyAction>
+> => (
   <TopNavLayout topNavButton={{ text: 'Templates', path: '/user-templates' }}>
-    <Home handleClick={handleClick} />
+    <Home />
   </TopNavLayout>
 )
 
-const mapDispatchToProps = (dispatch: Dispatch<AnyAction>): any => ({
-  handleClick: (): void => undefined,
-})
-
-export default connect(null, mapDispatchToProps)(HomeContainer)
+export default connect(null, null)(HomeContainer)
