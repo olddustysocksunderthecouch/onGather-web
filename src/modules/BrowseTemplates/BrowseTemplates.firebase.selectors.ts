94847b8cdac2beb65eb24c561783ec47e04cc5e7
Adrian Bunge <adrian@nonacreative.com>
2020-07-03 22:55:28 +0200

    feat(browser-templates): rudimentary shuffling of templates

--- src/modules/BrowseTemplates/BrowseTemplates.firebase.selectors.ts
+++ src/modules/BrowseTemplates/BrowseTemplates.firebase.selectors.ts
@@ -3,6 +3,14 @@ import { selectors as firestoreSelectors } from '../../common/modules/firestore'
 import { Template } from '../../common/types'
 import { selectActiveCategory } from './BrowseTemplates.selectors'
 
+function shuffleArray(array: any): any {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
+
 const arrayResult = (data: any): Template[] => {
   const array = Object.keys(data).map((id) => {
     return { templateId: id, ...data[id] }
@@ -31,7 +39,7 @@ export const selectTemplateForCategory = createSelector(
     if (data.browseTemplates) {
       const templates = arrayResult(data.browseTemplates)
       return activeCategory === 'All'
-        ? templates
+        ? shuffleArray(templates)
         : filterByCategory(templates, activeCategory)
     } else {
       return []
